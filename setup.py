fe83fcb8c55b5ee4c9010d160c499d36bfc24b8d
f03lipe <pires.a.felipe@gmail.com>
2013-02-25 04:03:32 -0300

    interface changed

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "toml-python",
+    version = "0.2",
+    description = "TOML parser for python.",
+    url = "https://github.com/f03lipe/toml-python",
+    license = "MIT License",
+    classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+    ],
+    packages = find_packages(),
+)
